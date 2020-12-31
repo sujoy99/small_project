70fd19221a17996fc1393a36b5af895b65ad845a
sujoy99 <48888636+sujoy99@users.noreply.github.com>
2020-12-31 18:33:36 +0600

    Create sidebar1.php

---
+++ sidebar1.php
@@ -0,0 +1,16 @@
+<!-- sidebar starts  -->
+
+<div class="sidebar">
+
+    <div class="logo d-flex justify-content-center align-items-center">
+        <img src="./image/ITbl.png" alt="" height="90px" width="90px">
+    </div>
+    <div class="sidebar_item">
+        <h1 class="text-white p-3 logo" style="font-size:34px;">Dashboard</h1>
+        <li><a href="./dashboard.php"><i class="fas fa-home pr-2"></i>Home</a></li>
+        <li><a href="./prepaid_cust_entry.php"><i class="fas fa-book"></i> Prepaid Customer Entry</a></li>
+        <li><a href="./logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+    </div>
+</div>
+
+<!-- sidebar ends  -->
